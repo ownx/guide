d5ca16f76bcd45239ba7f8d38ace936374243929
wangshudong <wsd312@163.com>
2017-07-13 23:02:29 +0800

    添加幻灯片

---
+++ vue/learnvue/src/views/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    <Swipe/> 
+  </div>
+</template>
+
+
+<script>
+
+import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import Swipe from '@/components/Swipe'
+Vue.use(VueAwesomeSwiper)
+
+  export default {
+    components: {
+      Swipe,
+    }
+  }
+</script>
+
+
